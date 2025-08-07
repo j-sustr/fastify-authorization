ac009021ecfc7efc15b4afff0c900b7bf6566691
Jan Šustr <jan.sustr11@gmail.com>
2025-08-07 16:10:22 +0200

    docs

--- src/web/plugins/core/api-docs.ts
+++ src/web/plugins/core/api-docs.ts
@@ -29,20 +29,9 @@ export const apiDocs: OpenAPIV3_1.Document<{}> = {
 };
 
 export default fp(async function (fastify) {
-  /**
-   * A Fastify plugin for serving Swagger (OpenAPI v2) or OpenAPI v3 schemas
-   *
-   * @see {@link https://github.com/fastify/fastify-swagger}
-   */
   await fastify.register(fastifySwagger, {
-    hideUntagged: true,
-    openapi: {
-      info: {
-        title: "Fastify demo API",
-        description: "The official Fastify demo API",
-        version: "0.0.0",
-      },
-    },
+    // hideUntagged: true,
+    openapi: apiDocs,
   });
 
   await fastify.register(import("@scalar/fastify-api-reference"), {
