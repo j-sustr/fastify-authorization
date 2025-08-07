68cd49a64dae02972649526f9c5e51b655f69c98
Jan Šustr <jan.sustr11@gmail.com>
2025-08-07 12:22:13 +0200

    endpoints

---
+++ src/web/routes/v1/protected/profile/endpoint.ts
@@ -0,0 +1,9 @@
+
+import type { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
+
+export default async function endpoint(fastify: FastifyInstance, options: FastifyPluginOptions) {
+    fastify.get("/", async (_request: FastifyRequest, reply: FastifyReply) => {
+
+        reply.code(200).send({ message: "Profile" });
+    });
+}
